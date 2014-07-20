96d32992ddc7ab6eaa4bb8bb1086208819d3d72c
Konstantin-Fridman <Konstantin-Fridman@users.noreply.github.com>
2014-07-20 13:24:56 +0300

    Update and rename Number Of Pages Allocated Per Row.sql to Number Of Pages Allocated Per Object.sql

--- Number Of Pages Allocated Per Object.sql
+++ Number Of Pages Allocated Per Object.sql
@@ -1,3 +1,3 @@
-SELECT [Number Of Pages Allocated Per Row] = in_row_reserved_page_count
+SELECT [Number Of Pages Allocated Per Object] = in_row_reserved_page_count
 	 , [Object Name] = OBJECT_NAME(object_id)
   from sys.dm_db_partition_stats
